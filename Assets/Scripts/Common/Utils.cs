a5e777d129a5b6dab82acc8424279eb9f607b067
KhurramSharif <Khurramblogs@gmail.com>
2019-03-12 20:05:11 +0500

    Commit

--- Assets/Scripts/Common/Utils.cs
+++ Assets/Scripts/Common/Utils.cs
@@ -1,40 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Utils : MonoBehaviour {
-	
-	private static Utils _instance;
-	public static Utils Instance()
-	{
-		if (_instance != null)
-		{
-			return _instance;
-		}
-		else
-		{
-			Debug.Log("Utils Not Inetilized....");
-		}
+public class Utils : MonoBehaviour
+{
+    private static Utils _instance;
 
-		return null;
-	}
+    private void Start()
+    {
+        _instance = this;
+    }
 
-	public static void SetFloat(string key, float value)
-	{
-		PlayerPrefs.SetFloat(key, value);
-	}
+    public static Utils Instance()
+    {
+        if (_instance != null)
+        {
+            return _instance;
+        }
+        else
+        {
+            Debug.Log("Utils Not Inetilized....");
+        }
 
-	public static float GetFloat(string key)
-	{
-		return PlayerPrefs.GetFloat(key);
-	}
-	
-	
-	public void UpdateBestTime(float value)
-	{
-		if(GetFloat(Constants.BestTime)<value)
-		{
-			SetFloat(Constants.BestTime, value);
-		}
-	}
+        return null;
+    }
+
+    public static void SetFloat(string key, float value)
+    {
+        PlayerPrefs.SetFloat(key, value);
+    }
+
+    public static float GetFloat(string key)
+    {
+        return PlayerPrefs.GetFloat(key);
+    }
+
+
+    public void UpdateBestTime(float value)
+    {
+        try
+        {
+            if (GetFloat(Constants.BestTime) < value)
+            {
+            }
+            else
+            {
+                SetFloat(Constants.BestTime, value);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 }
